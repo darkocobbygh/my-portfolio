390246f65244b2cb1423ac0ea61fd77c66765e68
darkocobbygh <darkogaucho@gmail.com>
2023-04-24 12:14:45 +0000

    Made few changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -16,13 +16,15 @@ const About=()=>{
             <h1 className='a-title'>About Me</h1>
             <p className='a-sub'></p>
             <p className='a-desc'>I’m a nice fun and friendly person, I’m honest and punctual, I work well in a team but also on my own as I like to set myself goals which I will achieve, I have good listening and communication skills. I have a creative mind and am always up for new challenges. I am well organized and always plan ahead to make sure I manage my time well.</p>
-                <h1>My Resume/Curriculum Vitae</h1>
+                <div className='cv'>
+                <h3>My Resume/Curriculum Vitae</h3>
                 <p>Click below to download my Resume/ Curriculum Vitae</p>
                         <a href={CV} download={CV}>
                             <button className="btn-highlighted-btn">
                                 <img src={Dw} alt=""className='dw-sv' /><br /> Download CV here
                             </button>
                         </a>
+                </div>
         </div>
     </div>
     )
